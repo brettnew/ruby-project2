5a6b96fad3b6bee08acb4a1a51910252d9f3aa9e
Brett New <brett.new91@gmail.com>
2016-07-08 11:39:40 -0700

    Add two more paths to route between words and adding words on app.rb

--- app.rb
+++ app.rb
@@ -2,10 +2,15 @@ require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/word")
+require("./lib/dealership")
 
 get("/") do
   @words = Word.all()
   erb(:index)
+end
+
+get('/words/new') do
+  erb(:word)
 end
 
 post("/words") do
