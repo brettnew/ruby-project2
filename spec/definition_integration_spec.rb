e37a21898d349a2b2df3ef0b69e164eea148b4a1
Brett New <brett.new91@gmail.com>
2016-07-08 15:31:23 -0700

    Add integration specs for all possible user input words.

--- spec/definition_integration_spec.rb
+++ spec/definition_integration_spec.rb
@@ -9,4 +9,16 @@ describe('the input word path', {:type => :feature}) do
     click_link('Add New Word')
     expect(page).to have_content("Add a word here")
   end
+  it ('allows user to enter word and reutrns the word after clicking button') do
+    visit('/words/new')
+    fill_in('new_word', :with => 'hello')
+    click_button('Add Word')
+    expect(page).to have_content('hello')
+  end
+  it ('allows user to return to main page') do
+    visit('/words/new')
+    click_link('Return to Main Page')
+    expect(page).to have_content('Welcome to
+    Your')
+  end
 end
