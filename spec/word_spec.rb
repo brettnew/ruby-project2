32493493f7b828325c376117af798a5f3a901b83
Brett New <brett.new91@gmail.com>
2016-07-08 09:12:51 -0700

    Add code to layout.erb and create css stylesheet

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -43,13 +43,13 @@ describe(Word) do
     end
   end
 
-  # describe("#add_definition") do
-  #   it("adds a new definition to the word") do
-  #     test_word = Word.new("love")
-  #     test_definition = Definition.new("an intense feeling of deep affection")
-  #     test_word.add_definition(test_definition)
-  #     expect(test_word.definitions()).to(eq([test_definition]))
-  #   end
-  # end
+  describe("#add_definition") do
+    it("adds a new definition to the word") do
+      test_word = Word.new("love")
+      test_definition = Definition.new("an intense feeling of deep affection")
+      test_word.add_definition(test_definition)
+      expect(test_word.definitions()).to(eq([test_definition]))
+    end
+  end
 
 end
